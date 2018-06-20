20adf1dea634393bbc1dabfce8ea9af403c00ffb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 16:04:14 -0700

    added check input and get input functions.

--- calculator.py
+++ calculator.py
@@ -7,10 +7,37 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# Your code goes here
 print("Welcome to our calculator")
-numbers = input("Please enter your desired action, with operator first. i.e. add 2 3. \n: ")
-numbers = numbers.rstrip()
-tokens = numbers.split(" ")
-print(tokens)
 
+
+def get_input():
+    """Get's initial user input and tokenizes."""
+    numbers = input("Enter action, with operator first. i.e. add 2 3. \n: ")
+    numbers = numbers.rstrip()
+    tokens = numbers.split(" ")
+    print(tokens)
+    return tokens
+
+
+tokens = get_input()
+
+
+def check_input():
+    """Checking user input for correct type and format."""
+    while True:
+        if tokens[0].isalpha():
+            for num in tokens[1:]:
+                if num.isdigit():
+                    return False
+                else:
+                    print("Please type digits aka 1, -4, 1000.")
+                    get_input()
+        else:
+            print("Please choose action from the menu.")
+            # Print menu
+            get_input()
+
+    print("Excellent, let me think for a moment...")
+
+
+check_input()
